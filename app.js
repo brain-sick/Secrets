f980cad0884eb7a0c4cfe95e167462b9f04cb5fe
adraj <t-adraj@vrbo.com>
2021-04-03 19:45:56 +0530

    Addded hashing authentication

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 const app = express();
 
 app.use(express.static("public"));
@@ -21,9 +21,6 @@ const userSchema = new mongoose.Schema({
   name: String,
   password: String,
 });
-
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = mongoose.model("User", userSchema);
 app.get("/", function (req, res) {
@@ -41,7 +38,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
   const user = new User({
     name: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
   user.save(function (err) {
     if (err) console.log(err);
@@ -51,7 +48,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   User.findOne({ name: username }, function (err, foundUser) {
     if (err) console.log(err);
     else {
